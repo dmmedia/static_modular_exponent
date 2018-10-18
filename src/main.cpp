40d29c2e23fc9e14996958445c436f5ef17be45d
Denis Frolov <dmmedia@dmmedia.org>
2018-10-18 13:21:48 +0300

    Cleaned up code, added documentation and examples

--- src/main.cpp
+++ src/main.cpp
@@ -1,45 +1,6 @@
-//#include "static_modexp.hpp"
+#include "static_modexp.hpp"
 #include <iostream>
-#include <tuple>
 
-template<class ArgsType>
-constexpr auto power_mod(const ArgsType args) {
-    constexpr auto bv = std::get<0>(args());
-    static_assert(bv > 0);
-    static_assert(bv < 1000000000);
-	
-    constexpr auto pv = std::get<1>(args());
-    static_assert(pv > 0);
-    static_assert(pv < 1000000);
-	
-    constexpr auto mv = std::get<2>(args());
-    static_assert(mv > 0);
-    static_assert(mv < 1000000000);
-	
-    u_int64_t res = 1u;
-    u_int64_t tmp = bv % mv;
-    u_int32_t pwr = pv;
-	
-    while (pwr != 0)
-    {
-        if (pwr & 1u)
-        {
-	    res = (res * tmp) % mv;
-	}
-	tmp = (tmp * tmp) % mv;
-	pwr >>= 1u;
-    }
-	
-    return res;
-}
-
-constexpr auto make_args = [](const u_int32_t b, const u_int32_t p, const u_int32_t m) constexpr
-{
-    return std::make_tuple(b, p, m);
-};
-
-#define INIT_ARGS(B, P, M) []{return std::make_tuple<u_int32_t>(B, P, M);}
-    
 int main(void)
 {
 //    constexpr auto args = make_args(131999920, 234979, 991540365);
--- src/static_modexp.hpp
+++ src/static_modexp.hpp
@@ -1,42 +1,68 @@
 #pragma once
 
-#include <cmath>
+/**
+ * \file static_modexp.hpp
+ * \brief Compile-time exponent module calculation
+ *
+ * This file contains a function \a power_mod which is able to calculate an
+ * exponent module during compilation time.
+ * Utility macro \a INIT_ARGS helps to initialize function parameters easily.
+ */
+
 #include <assert.h>
+#include <tuple>
 
-long double calculateModularExponent(const u_int32_t b, const u_int32_t p, const u_int32_t m)
-{
-    return fmod(pow((long double)b, (long double)p), (long double)m);
-}
+/**
+ * \def INIT_ARGS(B, P, M)
+ * \brief packs parameters into an std::tuple and into lambda expression
+ * \param B Base number
+ * \param P Power to raise the base to
+ * \param M Modulus
+ */
+#define INIT_ARGS(B, P, M) []{return std::make_tuple<u_int32_t>(B, P, M);}
 
-constexpr u_int32_t powermod(const u_int32_t b, const u_int32_t p, const u_int32_t m)
-{
-    // init const
-    constexpr auto base = b;
-    constexpr auto power = p;
-    constexpr auto modulo = m;
-    
-    // check ranges
-    static_assert(base > 0);
-    static_assert(base < 1000000000u);
-    
-    static_assert(power > 0);
-    static_assert(power < 1000000u);
-    
-    static_assert(modulo > 0);
-    static_assert(modulo < 1000000000u);
-    
+/**
+ * \fn power_mod(args)
+ * \brief Calculates an exponent module at compilation time
+ * \param args Lambda expression, wrapping std::tuple returning expression with three u_int32_t values: base, power and modulus
+ *
+ * Function power_mod() is able to perform compile-time modular exponentiation
+ * which reduces run-time computation overhead.
+ * Macro \a INIT_ARGS can be used for convenienlty passing values to this function.
+ * Values are limited as follows:
+ * Base and modulus should be greater than 0 and less than 10^9
+ * Power should be greater than 0 and less than 10^6
+ *
+ * Example usage:
+ *	auto result = power_mod(INIT_ARGS(98634296, 43280, 643026));
+ */
+template<class ArgsType>
+constexpr auto power_mod(const ArgsType args) {
+    constexpr auto bv = std::get<0>(args());
+    static_assert(bv > 0);
+    static_assert(bv < 1000000000);
+	
+    constexpr auto pv = std::get<1>(args());
+    static_assert(pv > 0);
+    static_assert(pv < 1000000);
+	
+    constexpr auto mv = std::get<2>(args());
+    static_assert(mv > 0);
+    static_assert(mv < 1000000000);
+	
     u_int64_t res = 1u;
-    u_int64_t tmp = base % modulo;
-    u_int32_t pwr = power;
+    u_int64_t tmp = bv % mv;
+    u_int32_t pwr = pv;
+	
     while (pwr != 0)
     {
-	if (pwr & 1u)
-	{
-	    res = (res * tmp) % modulo;
+        if (pwr & 1u)
+        {
+	    res = (res * tmp) % mv;
 	}
-	tmp = (tmp * tmp) % modulo;
-	pwr >>= 1;
+	tmp = (tmp * tmp) % mv;
+	pwr >>= 1u;
     }
-    
+	
     return res;
 }
